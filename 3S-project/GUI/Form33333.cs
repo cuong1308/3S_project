5b3568afd9234930f16ce0ad0a1ccb7feeee85e4
ASUS <xuanthien8501@gmail.com>
2022-05-05 15:36:47 +0700

    ád
    Signed-off-by: ASUS <xuanthien8501@gmail.com>

---
+++ 3S-project/GUI/Form33333.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3S_project.GUI
+{
+    public partial class Form33333 : Form
+    {
+        public Form33333()
+        {
+            InitializeComponent();
+        }
+    }
+}
