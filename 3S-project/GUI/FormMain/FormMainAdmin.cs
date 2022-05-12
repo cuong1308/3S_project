dfed52b927d02bee603f819789a7bbbb6133a280
ASUS <xuanthien8501@gmail.com>
2022-05-12 08:23:18 +0700

    thêm button cho admin

--- 3S-project/GUI/FormMain/FormMainAdmin.cs
+++ 3S-project/GUI/FormMain/FormMainAdmin.cs
@@ -21,5 +21,65 @@ namespace _3S_project.GUI.FormMain
         {
 
         }
+
+        private void đổiMậtKhẩuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quảnLýLớpToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quảnLýLớpHọcPhầnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quảnLýToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quảnLýMônHọcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quảnLýGiảngViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quảnLýSinhViênToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quảnLýKhoaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void theoTrungBìnhMônToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void theoMônHọcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void theoLớpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
